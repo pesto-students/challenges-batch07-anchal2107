744683dbe888d11307ec3e75630ad99b4636e813
Anchal Agarwal <46757207+anchal2107@users.noreply.github.com>
2021-02-15 18:29:10 +0530

    update function with partial solution

--- js-exercises/remove-falsy-values/removeFalsyValues.js
+++ js-exercises/remove-falsy-values/removeFalsyValues.js
@@ -1,8 +1,11 @@
-
 function removeFalsyValues(array) {
-
+  let resultArray=[];
+   array.filter((element) => {
+    if (element == null || isNaN(element)|| element == undefined || element ==false) {
+   console.log(element);
+    }
+    else{return resultArray.push(element);}
+  });
+  return resultArray;
 }
-
-export {
-  removeFalsyValues,
-};
+export { removeFalsyValues };
